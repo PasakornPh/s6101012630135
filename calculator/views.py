c1aa54df56340463c607ccdd9b176792d59e48d6
Pasakorn Phareyart <s6101012630135@email.kmutnb.ac.th>
2020-02-27 14:57:23 +0700

    add function add sub multi div in calculator_post

--- calculator/views.py
+++ calculator/views.py
@@ -14,12 +14,84 @@ def about(request):
 def calc_post(request):
     x = ''
     y = ''
+    operation = ''
     result = 0
-    if request.method == 'POST':
+
+    if 'plus' in request.POST:
 
         x = request.POST['number_x']
         y = request.POST['number_y']
-        print(x,y)
+        #print(x,y)
 
-        result = int(x) + int(y)
-    return render(request, 'calc_post.html',{'x':x , 'y':y,'result':result})
+        operation = ' + '
+
+        if x == '':
+            x = 0
+        elif y == '':
+            y = 0
+
+        result = float(x) + float(y)
+        if str(result).split('.')[1] == '0':
+            result = "{:.0f}".format(result)
+        else:
+            pass
+
+    elif 'sub' in request.POST:
+
+        x = request.POST['number_x']
+        y = request.POST['number_y']
+        #print(x,y)
+
+        operation = ' - '
+
+        if x == '':
+            x = 0
+        elif y == '':
+            y = 0
+
+        result = float(x) - float(y)
+        if str(result).split('.')[1] == '0':
+            result = "{:.0f}".format(result)
+        else:
+            pass
+
+    elif 'multi' in request.POST:
+
+        x = request.POST['number_x']
+        y = request.POST['number_y']
+        #print(x,y)
+
+        operation = ' * '
+
+        if x == '':
+            x = 0
+        elif y == '':
+            y = 0
+
+        result = float(x) * float(y)
+        if str(result).split('.')[1] == '0':
+            result = "{:.0f}".format(result)
+        else:
+            pass
+
+
+    elif 'div' in request.POST:
+
+        x = request.POST['number_x']
+        y = request.POST['number_y']
+        #print(x,y)
+
+        operation = ' / '
+
+        if x == '':
+            x = 0
+        elif y == '':
+            y = 0
+
+        result = float(x) / float(y)
+        if str(result).split('.')[1] == '0':
+            result = "{:.0f}".format(result)
+        else:
+            pass
+
+    return render(request, 'calc_post.html', {'x': x, 'y': y, 'result': result,'operation' : operation})
